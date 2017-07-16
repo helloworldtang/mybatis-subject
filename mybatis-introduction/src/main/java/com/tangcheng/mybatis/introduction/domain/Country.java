2daaa586f3afb25c750c19ef496194ce1c71c54c
Tangcheng <helloworld.tang@qq.com>
2017-07-16 23:45:56 +0800

    init

---
+++ mybatis-introduction/src/main/java/com/tangcheng/mybatis/introduction/domain/Country.java
@@ -0,0 +1,33 @@
+package com.tangcheng.mybatis.introduction.domain;
+/**
+ * Created by tangcheng on 7/16/2017.
+ */
+public class Country {
+    private Long id;
+    private String name;
+    private String code;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
---
+++ mybatis-introduction/src/test/java/com/tangcheng/mybatis/introduction/mapper/CountryMapperTest.java
@@ -0,0 +1,56 @@
+package com.tangcheng.mybatis.introduction.mapper;
+
+import com.tangcheng.mybatis.introduction.domain.Country;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tangcheng on 7/16/2017.
+ */
+public class CountryMapperTest {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(CountryMapperTest.class);
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+    @BeforeClass
+    public static void init() {
+        try {
+            Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        } catch (IOException e) {
+            LOGGER.error(e.getMessage(),e);
+        }
+    }
+
+    @Test
+    public void testSelectAll() {
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            List<Country> countryList = sqlSession.selectList("selectAll");
+            for (Country country : countryList) {
+                Long id = country.getId();
+                String name = country.getName();
+                String code = country.getCode();
+                LOGGER.info("{},{},{}", id, name, code);
+                System.out.printf("%-4d%4s%4s \n",id,name,code);
+            }
+
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+}
