3503b9c3dc8cfa37f4de7cf31adf728e2c918bcc
tang.cheng <tang.cheng@xiaoyi.com>
2017-07-17 13:08:30 +0800

    add sql

---
+++ mybatis-introduction/db/v1.0.0.sql
@@ -0,0 +1,28 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 100113
+Source Host           : localhost:3306
+Source Database       : mybatis-subject
+
+Target Server Type    : MYSQL
+Target Server Version : 100113
+File Encoding         : 65001
+
+Date: 2017-07-17 12:50:57
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for country
+-- ----------------------------
+DROP TABLE IF EXISTS `country`;
+CREATE TABLE `country` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `code` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+SET FOREIGN_KEY_CHECKS=1;
