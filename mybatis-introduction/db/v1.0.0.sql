5c07b43a3aaed5b7aae365610c7200919b218ce7
Tangcheng <helloworld.tang@qq.com>
2017-07-17 23:40:02 +0800

    sql

--- mybatis-introduction/db/v1.0.0.sql
+++ mybatis-introduction/db/v1.0.0.sql
@@ -12,6 +12,8 @@ File Encoding         : 65001
 
 Date: 2017-07-17 12:50:57
 */
+CREATE DATABASE `mybatis-subject` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `mybatis-subject`;
 
 SET FOREIGN_KEY_CHECKS=0;
 
@@ -21,8 +23,16 @@ SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS `country`;
 CREATE TABLE `country` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_bin NOT NULL,
-  `code` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `code` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-SET FOREIGN_KEY_CHECKS=1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of country
+-- ----------------------------
+INSERT INTO `country` VALUES ('1', '中国', 'CN');
+INSERT INTO `country` VALUES ('2', '美国', 'US');
+INSERT INTO `country` VALUES ('3', '俄罗斯', 'RU');
+INSERT INTO `country` VALUES ('4', '英国', 'GB');
+INSERT INTO `country` VALUES ('5', '法国', 'FR');
